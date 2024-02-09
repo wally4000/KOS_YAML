a245ee8493cec0063d87b51217b105d071cb25bc
Wally4000 <catapultit@me.com>
2024-02-09 12:28:52 +1100

    Copy the prebuilt arm driver.

--- 004_kos.sh
+++ 004_kos.sh
@@ -4,3 +4,5 @@ cp $DC_ROOT/kos/doc/environ.sh.sample $DC_ROOT/kos/environ.sh
 source $DC_ROOT/kos/environ.sh
 make clean
 make -j16
+
+#ARM_CC_IS_AVAILABLE value should be set here to build the stream.drv for now let's set it manually (kernel/arch/dreamcast/sound/arm)
